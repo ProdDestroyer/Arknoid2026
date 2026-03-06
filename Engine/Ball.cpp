c6db7e73dc0d513adc901056118edace382e5108
ProdDestroyer <samuelfelipelopez@gmail.com>
2026-03-06 10:17:38 -0500

    Ball against Wall colission logic and sound implemented

--- Engine/Ball.cpp
+++ Engine/Ball.cpp
@@ -3,7 +3,8 @@
 
 Ball::Ball(Vec2D ballCenter) :
 	center(ballCenter),
-	direction(1.0f, -2.0f)
+	direction(1.0f, -2.0f),
+	soundPad(L"Sounds\\arkpad.wav")
 {
 	direction.Normalize();
 }
@@ -11,6 +12,32 @@ Ball::Ball(Vec2D ballCenter) :
 void Ball::Move(const float dt)
 {
 	center += direction * ( dt * speed);
+	CheckWallsCollision();
+}
+
+void Ball::CheckWallsCollision()
+{
+	if (center.x + radius > Graphics::ScreenWidth) {
+		center.x = Graphics::ScreenWidth - radius;
+		direction.x *= -1;
+		soundPad.Play();
+
+	} else if (center.x - radius < 0) {
+		center.x = radius;
+		direction.x *= -1;
+		soundPad.Play();
+	 }
+
+	if (center.y + radius > Graphics::ScreenHeight) {
+		center.y = Graphics::ScreenHeight - radius;
+		direction.y *= -1;
+		soundPad.Play();
+	}
+	else if (center.y - radius < 0) {
+		center.y = radius;
+		direction.y *= -1;
+		soundPad.Play();
+	}
 }
 
 void Ball::Render(Graphics& gfx) const
--- Engine/Ball.h
+++ Engine/Ball.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "Vec2D.h"
 #include "Graphics.h"
+#include "Sound.h"
 class Ball {
 public:
 	Ball(Vec2D ballCenter);
 	void Move(const float dt);
+	void CheckWallsCollision();
 	void Render(Graphics& gfx) const;
-	static constexpr float radius = 14.0f;
-	static constexpr float speed = 60.0f;
+	static constexpr float radius = 7.0f;
+	static constexpr float speed = 240.0f;
 private:
 	Vec2D center;
 	Vec2D direction;
+	Sound soundPad;
 };
